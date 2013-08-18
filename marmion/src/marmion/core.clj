9757f972011c81102d818c4a8a7c2a30369a4001
sam atman <atmanistan@gmail.com>
2013-08-18 14:25:07 -0700

    failure rate is good, not great

--- marmion/src/marmion/core.clj
+++ marmion/src/marmion/core.clj
@@ -16,6 +16,10 @@
 (def parsed-migraine (marmalade-parser (slurp "migraine.md")))
 
 (def test-vec (apply vector (clojure.string/split (slurp "test-files.txt") #"\n")))
+
+(def fail-vec (apply vector (clojure.string/split (slurp "fail-files.txt") #"\n")))
+
+(defn count-failures [x] (count (insta/parses edn (slurp (str "/Users/atman/code/opp/edn-tests/invalid-edn/" x)))))
                                                   
 (defn count-parses [x] (count (insta/parses edn (slurp (str "/Users/atman/code/opp/edn-tests/valid-edn/" x)))))
 
