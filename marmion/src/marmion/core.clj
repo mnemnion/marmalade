3031d5c7857167c8f85e795f129bea6aacdebeb2
sam atman <atmanistan@gmail.com>
2013-10-16 16:05:09 -0700

    shivering with antici...wait for it, wait for it......

--- marmion/src/marmion/core.clj
+++ marmion/src/marmion/core.clj
@@ -24,15 +24,39 @@
   "takes a source string, an anchor string, and an expansion string.
 if source and anchor are equal, return a map {:tag :expanded :content 'expansion'.
 otherwise, return the anchor string."
-  [source anchor expansion]
-  (if (= source anchor)
+  [source-string anchor expansion]
+  (if (= source-string anchor)
     (assoc {:tag :expanded} :content expansion)
     (assoc {:tag :marmalade/error} :content  anchor)))
 
 
 (defn expand-anchor-if-equal
-  [source tangle expansion]
+  [source-string tangle expansion]
 
   (insta/transform {:anchor
-                    (fn [& chars] (transform-if-equal source (first chars) expansion))}
+                    (fn [& chars] (transform-if-equal source-string
+                                                     (first chars)
+                                                     expansion))}
                    tangle))
+
+(defn get-expansion
+  "takes a :source tangle and returns the code-body as a string"
+  [tangle]
+  (flat-tree (tag-stripper :code-body tangle)))
+
+(defn get-source-macro
+  "takes a :source tangle and returns the macro as a string"
+  [tangle]
+  (first (:content (first (tag-stripper :source tangle)))))
+
+(defn expand-source
+  "takes a :source tangle and maps it across a file-map. expands into anchors,
+returning file-map."
+  [source file-map]
+  (map #(assoc {}
+          (nth % 0)
+          (expand-anchor-if-equal
+           (get-source-macro source)
+           (nth % 1)
+           (get-expansion source)))
+       file-map))
